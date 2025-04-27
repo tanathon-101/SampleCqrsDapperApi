672d06a5e3b37a2c4c860d06c598d859036ff526
tanathon-101 <tanaton.akko@gmail.com>
2025-04-27 11:20:03 +0700

    CreateCustomerCommandValidator

---
+++ SampleCqrsDapperApi.Application/Validators/CreateCustomerCommandValidator.cs
@@ -0,0 +1,19 @@
+using FluentValidation;
+using SampleCqrsDapperApi.Application.Commands;
+
+namespace SampleCqrsDapperApi.Application.Validators
+{
+    public class CreateCustomerCommandValidator : AbstractValidator<CreateCustomerCommand>
+    {
+        public CreateCustomerCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(100).WithMessage("Name must not exceed 100 characters.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Email must be a valid email address.");
+        }
+    }
+}
